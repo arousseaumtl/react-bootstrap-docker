3a03b0c4f769078dd4125f8348ab79cf7053beec
Alexander Rousseau <alexander.rousseau@plusgrade.com>
2022-08-29 11:46:58 -0400

    small fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,7 @@ if [[ "$(ls -A app/ | wc -w | xargs)" -gt "1" ]]
 then
   npm start --prefix ./app
 else
-  rm app/.gitkeep && \
+  rm app/.gitkeep; \
   yes | npx create-react-app app && \
   npm start --prefix ./app
 fi
