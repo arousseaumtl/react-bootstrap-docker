9defb3d59dc9d70e70202abc5eaa01678f752e8b
Alexander Rousseau <alexander.rousseau@plusgrade.com>
2022-08-29 23:53:41 -0400

    updates

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-if [[ "$(ls -A app/ | wc -w | xargs)" -gt "0" ]]
+if [[ "$(ls -A app/ | wc -w | xargs)" -gt "1" ]]
 then
   npm start --prefix ./app
 else
+  rm app/.gitkeep;
   yes | npx create-react-app app && \
   npm start --prefix ./app
 fi
