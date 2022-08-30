b15eb432b57699cbb0e42af8603a25ffeeba5444
Alexander Rousseau <alexander.rousseau@plusgrade.com>
2022-08-29 23:50:43 -0400

    fix redundancies

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 
-if [[ "$(ls -A app/ | wc -w | xargs)" -gt "1" ]]
+if [[ "$(ls -A app/ | wc -w | xargs)" -gt "0" ]]
 then
   npm start --prefix ./app
 else
-  rm app/.gitkeep; \
   yes | npx create-react-app app && \
   npm start --prefix ./app
 fi
